0b08bc27307309eaf23321e67b5ba65cdec49bd6
Siva <sivasankar.sivakripa123@gmail.com>
2025-11-13 07:55:24 +0530

    version bump

--- test/declarate_test.dart
+++ test/declarate_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/declarate.dart';
+import 'package:declarate/declarate.dart';
 
 // ------------------------------------------------------------ //
 //  Mock Declarates for Testing
